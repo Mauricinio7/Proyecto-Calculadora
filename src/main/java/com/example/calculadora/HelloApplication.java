0528db011e76bdb9eaa27545438004f4cb362d2e
Mauricinio7 <miory.salas@hotmail.com>
2024-02-19 16:54:04 -0600

    Arreglo de errores de la GUI

--- src/main/java/com/example/calculadora/HelloApplication.java
+++ src/main/java/com/example/calculadora/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 368, 461);
-        stage.setTitle("Hello!");
+        stage.setTitle("Calculadora");
         stage.setScene(scene);
         stage.show();
     }
